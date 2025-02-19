48bd9daf362f529ce81626e57ff72af016da6330
Melanie Roger <mel29r@gmail.com>
2025-02-19 15:00:06 -0300

    update

--- tateti/mi-proyecto-react/src/components/tateti/Tateti.tsx
+++ tateti/mi-proyecto-react/src/components/tateti/Tateti.tsx
@@ -5,86 +5,94 @@ import { useEffect } from "react";
 
 const Tateti = () =>{
 
+  //States
     const [cantClicks, setCantClicks] = useState(0);
     const [ganador, setGanador] = useState("Empezar a jugar!");
     const [resultados, setResultados] = useState(Array(9).fill(""));
 
-
-    useEffect(() => {
-      if( cantClicks > 0 ){
-        const nuevocontenido = contenido();
-        evaluarJugada(nuevocontenido)
-      }
-        
-      
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[resultados, cantClicks]);
-
-
-    const turno = () => { setCantClicks((prev) => prev+1) };
-
-
+  // Set state
+    const turno = () => { setCantClicks((prev) => prev+1); console.log("TURNOOO, cantClicks: " + cantClicks)};
     const actualizarResultado = (index:number, nuevoValor:string) => {
       setResultados((prevResultados) => {
         const nuevoArray = [...prevResultados];
         nuevoArray[index] = nuevoValor;
+        console.log("cantClicks: " + cantClicks);
+        console.log("ActualizarResultado, nuevoValor: " + nuevoValor);
         return nuevoArray; 
       });
     };
-
-    const resetearVariables = () => {
-      setResultados(["", "", "", "", "", "", "", "", ""]);
-      setCantClicks(0);
-      setGanador("Empezar a jugar!");
-    };
-
-    const verificarClick = (position: number): boolean => (resultados[position]=== "" ? true : false);
-      
-    const contenido = () => (cantClicks % 2 === 0 ? "X" : "O");
-
-    const evaluarJugada = (contenido: string) => {
-      console.log(contenido);
-      if (
-          (resultados[0] === contenido && resultados[1] === contenido && resultados[2] === contenido) ||
-          (resultados[3] === contenido && resultados[4] === contenido && resultados[5] === contenido) ||
-          (resultados[6] === contenido && resultados[7] === contenido && resultados[8] === contenido) ||
-          (resultados[0] === contenido && resultados[3] === contenido && resultados[6] === contenido) ||
-          (resultados[1] === contenido && resultados[4] === contenido && resultados[7] === contenido) ||
-          (resultados[2] === contenido && resultados[5] === contenido && resultados[8] === contenido) ||
-          (resultados[0] === contenido && resultados[4] === contenido && resultados[8] === contenido) ||
-          (resultados[2] === contenido && resultados[4] === contenido && resultados[6] === contenido)
-      ) {
-          setGanador("Ganador: " + contenido);
-      } else if (!resultados.includes("")) {
-          resetearVariables();
+    const mensaje = () => {
+      const nuevoCont = contenido();
+      if(evaluarJugada(nuevoCont)){
+        setGanador("Ganador: " + nuevoCont);
+        resetearVariables();
+      }else{
+        setGanador("Sigue jugando")
       }
-  };
+    }
 
+  // Functions
+    const verificarClick = (position: number): boolean => (resultados[position]=== "" ? true : false);
+
+    const contenido = () => (cantClicks % 2 === 0 ? "☀️" : "🌑​​");
+
+    const evaluarJugada = (contentTurno: string) => {
+        console.log("cantClicks: " + cantClicks);
+        console.log("EvaluarJugada, contentTurno: " + contentTurno);
+        if (
+            (resultados[0] === contentTurno && resultados[1] === contentTurno && resultados[2] === contentTurno) ||
+            (resultados[3] === contentTurno && resultados[4] === contentTurno && resultados[5] === contentTurno) ||
+            (resultados[6] === contentTurno && resultados[7] === contentTurno && resultados[8] === contentTurno) ||
+            (resultados[0] === contentTurno && resultados[3] === contentTurno && resultados[6] === contentTurno) ||
+            (resultados[1] === contentTurno && resultados[4] === contentTurno && resultados[7] === contentTurno) ||
+            (resultados[2] === contentTurno && resultados[5] === contentTurno && resultados[8] === contentTurno) ||
+            (resultados[0] === contentTurno && resultados[4] === contentTurno && resultados[8] === contentTurno) ||
+            (resultados[2] === contentTurno && resultados[4] === contentTurno && resultados[6] === contentTurno)
+        ) {
+            return true
+        } else{
+          return false
+        }
+    };
 
     const acciones = (index: number) => { 
-      if (verificarClick(index)) {
-        const nuevocontenido = contenido();
-        actualizarResultado(index, nuevocontenido);
-        turno();
-      }
+        if (verificarClick(index)) {
+          console.log("Acciones");
+          
+          const nuevoCont = contenido();
+          console.log("cantClicks: " + cantClicks);
+          console.log("Acciones, nuevoCont: " + nuevoCont);
+          
+          actualizarResultado(index, nuevoCont);
+          turno();
+        }
     };
+
+    const resetearVariables = () => {
+        setResultados(["", "", "", "", "", "", "", "", ""]);
+        setCantClicks(0);
+    };
+
+  // useEffect
+    useEffect(() => {
+      if( cantClicks > 0 ){
+        mensaje();
+      }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[resultados]);
 
     return (
       <>
         <h1>{cantClicks}</h1>
+
         <div className="tateti">
-
           {resultados.map((value, index) => (
-
             <Cuadrado key={index} contenido={value} posicion={index} handleClick={() => acciones(index)} />
-
           ))}
+        </div>
 
         <h2>{ganador}</h2>
-
-        </div>
       </>
-
     );
 }
 
